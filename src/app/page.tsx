d330a8f85247f2cccd8cafb863a7aef5251bf40a
hin6150 <hin6150@gmail.com>
2024-01-06 17:10:47 +0900

    Feat - 그리드 오타 발견

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <div className="bg-bg  h-screen pt-52">
+      <div className="bg-bg h-screen pt-52">
         <Image
           src={headerImg}
           alt="별이 반짝이는 겨울 밤의 산 배경이미지입니다. 하늘색과 남색의 산들과 눈이 더해져 차가운 겨울 느낌을 줍니다."
@@ -40,6 +40,22 @@ export default function Home() {
             두런두런을 통해 친구들과 함께 도전하고 하루하루 작은 성공을
             모아보세요.
           </h2>
+        </div>
+      </div>
+      <div>
+        <div className="inner pt-44">
+          <div className="grid grid-cols-2 gap-10">
+            <div>
+              <h4 className="text-h4 font-h4 text-primary-1">목표설정</h4>
+              <h3 className="text-h3 font-h3">목표를 간편하게 관리하세요!</h3>
+              <p className="text-body1 font-body1 text-gray-1">
+                간단한 목표 설정 기능을 통해 하루를 계획하고 관리할 수 있어요.
+                매일 작은 목표를 설정하여 소소한 달성을 이루어 내고 나만의
+                루틴으로 만들어보세요!
+              </p>
+            </div>
+            <div></div>
+          </div>
         </div>
       </div>
     </div>
