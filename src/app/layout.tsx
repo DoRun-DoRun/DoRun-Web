fc00b6c69d1e90b920e630f347f740038b923752
joeunSong <thdwhdms99@naver.com>
2024-01-06 19:25:21 +0900

    Feat - main 3번째

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Noto_Sans_KR } from 'next/font/google';
-import Header from '@/components/header';
+import Link from 'next/link';
+import Image from 'next/image';
+import Logo from '/public/images/title.png';
 
 const notoSansKr = Noto_Sans_KR({
   weight: ['400', '500', '700'],
@@ -24,7 +26,60 @@ export default function RootLayout({
       <body className={notoSansKr.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
 }
+
+const NavBar = () => {
+  const category = [
+    { name: '내 캐릭터 찾기', link: '/character' },
+    { name: '고객센터', link: '/customer' },
+    { name: 'About us', link: '/aboutus' },
+    { name: '팀원 모집', link: 'team' },
+    { name: '소셜 로그인', link: 'login' },
+  ];
+
+  return (
+    <div className="flex flex-row gap-48">
+      {category.map((nav, idx) => (
+        <Link href={nav.link}>
+          <div key={idx} className="text-gray-4">
+            {nav.name}
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <div className="flex flex-col w-full h-80 justify-center absolute bg-white">
+      <div className="inner">
+        <div className="flex items-center justify-between w-full">
+          <Link href="/">
+            <Image src={Logo} alt="두런두런" />
+          </Link>
+          <NavBar />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <div className="flex flex-col w-full h-360 justify-center bg-gray-6">
+      <div className="inner">
+        <div className="flex items-center justify-between w-full">
+          <Link href="/">
+            <Image src={Logo} alt="두런두런" />
+          </Link>
+          <NavBar />
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,33 +5,34 @@ import playStoreLogo from '/public/images/play_store_logo.png';
 import Link from 'next/link';
 import { MdCheckCircle } from 'react-icons/md';
 import { MdMoreHoriz } from 'react-icons/md';
+import phone1 from '/public/images/play_image_1.png';
 
 export default function Home() {
   return (
     <div>
-      <div className="bg-bg h-screen pt-52">
+      <div className="bg-bg h-screen pt-208">
         <Image
           src={headerImg}
           alt="별이 반짝이는 겨울 밤의 산 배경이미지입니다. 하늘색과 남색의 산들과 눈이 더해져 차가운 겨울 느낌을 줍니다."
           className="w-screen absolute bottom-0"
         />
-        <div className="inner gap-4">
-          <div className="flex gap-4">
+        <div className="inner gap-16">
+          <div className="flex gap-16">
             <Link href="https://www.naver.com" target="_blank">
-              <div className="p-3 pl-5 pr-6 items-center bg-white rounded-lg flex gap-2 shadow-md">
+              <div className="p-12 pl-20 pr-24 items-center bg-white rounded-lg flex gap-8 shadow-md">
                 <Image
                   src={appleLogo}
                   alt="AppleStore을 나타내는 로고입니다.
 "
-                  className="w-7"></Image>
+                  className="w-28"></Image>
                 <p className="text-24 font-bold">App Store</p>
               </div>
             </Link>
-            <div className="p-3 pl-5 pr-6 items-center bg-white rounded-lg flex gap-2 shadow-md">
+            <div className="p-12 pl-20 pr-24 items-center bg-white rounded-lg flex gap-8 shadow-md">
               <Image
                 src={playStoreLogo}
                 alt="PlayStore을 나타내는 로고입니다."
-                className="w-7"></Image>
+                className="w-28"></Image>
               <p className="text-24 font-bold">Play Store</p>
             </div>
           </div>
@@ -44,27 +45,42 @@ export default function Home() {
           </h2>
         </div>
       </div>
-      <div className="h-360">
-        <div className="inner pt-44">
-          <div className="grid grid-cols-2 gap-10">
-            <div>
+      <div className="h-1440">
+        <div className="inner pt-176 gap-144">
+          <div className="grid grid-cols-2 gap-90">
+            <div className="flex flex-col gap-25">
               <h4 className="text-h4 font-h4 text-primary-1">목표설정</h4>
               <h3 className="text-h3 font-h3">목표를 간편하게 관리하세요!</h3>
               <p className="text-body1 font-body1 text-gray-1">
-                간단한 목표 설정 기능을 통해 하루를 계획하고 관리할 수 있어요.
-                매일 작은 목표를 설정하여 소소한 달성을 이루어 내고 나만의
-                루틴으로 만들어보세요!
+                간단한 목표 설정 기능을 통해 하루를 계획하고 <br />
+                관리할 수 있어요. 매일 작은 목표를 설정하여 <br />
+                소소한 달성을 이루어 내고 나만의 루틴으로 만들어보세요!
               </p>
             </div>
-            <div className="flex flex-col gap-10">
+            <div className="flex flex-col gap-40">
               <ChallengeItem background />
               <ChallengeItem />
               <ChallengeItem background />
               <ChallengeItem />
             </div>
           </div>
+          <div className="grid grid-cols-2 gap-86">
+            <Image src={phone1} alt="" className="pl-130" />
+            <div className="flex flex-col justify-center gap-25">
+              <h4 className="text-h4 font-h4 text-primary-1">도전과 경쟁</h4>
+              <h3 className="text-h3 font-h3">친구들과 함께 도전하세요!</h3>
+              <p className="text-body1 font-body1 text-gray-1">
+                캐릭터와 아이템을 통해 친구들과 상호작용하며
+                <br /> 더욱 즐겁게 챌린지를 수행할 수 있어요.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
+      <div className="h-928 bg-primary-2">
+        <div></div>
+      </div>
+      <div className="h-1600 "></div>
     </div>
   );
 }
@@ -72,13 +88,13 @@ export default function Home() {
 const ChallengeItem = ({ background }: { background?: boolean }) => {
   return (
     <div className={`relative ${!background && 'self-end'}`}>
-      <div className="gap-3 w-130 bg-glass1 py-5 px-4 flex justify-start items-center rounded-2xl shadow-glass z-10 relative">
+      <div className="gap-12 w-520 bg-glass1 py-20 px-16 flex justify-start items-center rounded-2xl shadow-glass z-10 relative">
         <MdCheckCircle size="32" color="#648CF3" />
         <p className="text-24 font-medium flex-1">챌린지 아이템</p>
         <MdMoreHoriz size="32" color="#B5B5B5" />
       </div>
       {background && (
-        <div className="w-130 h-16 bg-primary3 py-3 px-4 rounded-2xl absolute top-7 left-5 z-0"></div>
+        <div className="w-520 h-76 bg-primary3 px-16 rounded-2xl absolute top-28 left-20s z-0"></div>
       )}
     </div>
   );
